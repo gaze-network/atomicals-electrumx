54594ab0f71d56bd744f21c7681dda67fcd7edf1
Gaze <dev@gaze.network>
2024-03-25 11:37:18 +0700

    feat: implement event hash versioning

--- electrumx/__init__.py
+++ electrumx/__init__.py
@@ -2,6 +2,7 @@ __version__ = "0.1.0"
 version = f'gaze-electrumx-v{__version__}'
 version_short = __version__
 gaze_db_version = 1
+gaze_event_hash_version = 1
 
 from electrumx.server.controller import Controller
 from electrumx.server.env import Env
