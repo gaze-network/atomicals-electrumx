0c646c9bab1275a38b5095b32f6188c3c64c2e51
Gaze <dev@gaze.network>
2024-06-20 14:12:28 +0700

    chore: bump up version

--- electrumx/__init__.py
+++ electrumx/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "0.1.2"
+__version__ = "1.0.0"
 version = f'gaze-electrumx-v{__version__}'
 version_short = __version__
-gaze_db_version = 1
+gaze_db_version = 2
 gaze_event_hash_version = 1
